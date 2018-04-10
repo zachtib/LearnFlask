fec2e4941796afa05b4c3d20a8e5f6efdbbc2ed5
Zach Tibbitts <ztibbitts@hchb.com>
2018-04-10 09:15:31 -0400

    add logging

--- learnflask.py
+++ learnflask.py
@@ -1,7 +1,12 @@
+import logging
+import sys
 from flask import Flask
 
 app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.DEBUG)
 
 @app.route('/')
 def home():
+    app.logger.debug('Hello, World!')
     return 'Hello, World!'
