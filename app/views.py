a4eac827b651bb261431fcf3f0bbe494a4be70f1
Zach Tibbitts <ztibbitts@hchb.com>
2018-04-12 12:27:39 -0400

    wtf and LoginForm

--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
 from app import app
 
+from app.forms import LoginForm
+from app.models import User
+from flask import render_template, redirect, url_for
+from flask_login import current_user, login_user
+
+
 @app.route('/')
 def home():
     app.logger.debug('Hello, World!')
     return 'Hello, World!'
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('/'))
+    form = LoginForm()
+    return render_template('login.html', title='Sign in', form=form)
