1d87703495a2188ba8008d903ee9bba02e36c19a
Zach Tibbitts <ztibbitts@hchb.com>
2018-04-12 09:47:22 -0400

    env things. install migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,16 @@
 import logging
 import sys
 
+from config import Config
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.DEBUG)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import views
+from app import views, models
