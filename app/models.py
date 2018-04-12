90e7b98a81fa30f290f1f1bc6275bb227710ec6a
Zach Tibbitts <ztibbitts@hchb.com>
2018-04-12 12:12:04 -0400

    add flask_login

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
 from app import db
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
